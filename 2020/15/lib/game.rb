d4e34b7d5cd7c7017e6723944dfc024c5f5626e3
Jeremy Cole <jeremy@jcole.us>
2020-12-18 19:18:56 -0800

    Day 15 cleanup

--- 2020/15/lib/game.rb
+++ 2020/15/lib/game.rb
@@ -4,31 +4,35 @@ class Game
   def initialize(starting_numbers)
     @starting_numbers = starting_numbers
     @previous_numbers = {}
-    @turn = 0
+    @next_turn = 0
     @used_numbers = {}
     @last_number = nil
   end
 
   def turns_since_number(number)
-    @used_numbers[number][0] - @used_numbers[number][1]
+    used_number = @used_numbers[number]
+    used_number[0] - used_number[1]
   end
 
   def calculate_next_number
     return @starting_numbers.shift unless @starting_numbers.empty?
-    return 0 unless @used_numbers.include?(@last_number) && @used_numbers[@last_number].size > 1
+
+    used_number = @used_numbers[@last_number]
+    return 0 unless used_number && used_number.size > 1
+
     turns_since_number(@last_number)
   end
 
   def generate_next_number
-    this_number = calculate_next_number
-    this_turn = @turn
-    @used_numbers[this_number] ||= []
-    @used_numbers[this_number].unshift(@turn)
-    @used_numbers[this_number].pop if @used_numbers[this_number].size > 2
-    @last_number = this_number
-    @turn += 1
+    number = calculate_next_number
+    turn = @next_turn
+    used_number = @used_numbers[number] ||= []
+    used_number.unshift(@next_turn)
+    used_number.pop if used_number.size > 2
+    @last_number = number
+    @next_turn += 1
 
-    [this_number, this_turn]
+    [number, turn]
   end
 
   def each_number
